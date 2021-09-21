0d2b558e50918632df01842e41f3d2f756bb03da
max <lkg888888@163.c0m>
2021-09-21 11:40:47 +0800

    changed again

---
+++ src/test_demo_09/Test10.java
@@ -0,0 +1,14 @@
+package test_demo_09;
+
+public class Test10 {
+	
+	public static void main(String[] args) {
+		
+		Test10 t10 = new Test10();
+		
+		boolean result = t10 instanceof Test10; 
+		System.out.println(" result is " + result);
+		
+	}
+
+}
